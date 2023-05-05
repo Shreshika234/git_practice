ad41b7ae8702ba2dbdb6871ee22777744c76bd08
Shreshika234 <121421777+Shreshika234@users.noreply.github.com>
2023-05-05 17:18:30 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,26 @@
+class Line():
+
+    def __init__(self,coor1,coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+
+    def distance(self):
+         
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return  int(((x2-x1)**2+(y2-y1)**2)**0.5)
+        
+    
+    def slope(self):
+        
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return int((y2-y1)/(x2-x1))
+
+coor1 = (3,2)
+coor2 = (8,10)
+
+myline = Line(coor1,coor2)
+
+print(myline.distance())
+print(myline.slope())
